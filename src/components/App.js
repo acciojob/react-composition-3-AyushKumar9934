33de0161c75630dfa8ae50042934e69bc9e97f65
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-14 15:12:43 +0530

    p

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import Tooltip from "./Tooltip";
 
 const App = () => {
   const text = "Hover over me";
-  const text1="Hover over me to see another Tooltip"
-  
+  const text1 = "Hover over me to see another Tooltip";
+
   return (
     <div>
-       <Tooltip text={text}>
-         <h2 className="tooltip"><button>Hover over me</button></h2>
-       </Tooltip>
-       <Tooltip text={text1}>
-         <p className="tooltip"><button>Hover over me to see another Tooltip</button></p>
-       </Tooltip>
+      <div>
+        <Tooltip text={text}>
+          <h2 className="tooltip">
+            <p>This is a tooltip</p>
+          </h2>
+        </Tooltip>
+      </div>
+      <div>
+        <Tooltip text={text1}>
+          <h2 className="tooltip">
+            <p className="tooltip">Hover over me to see another Tooltip</p>
+          </h2>
+        </Tooltip>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App; 
+export default App;
